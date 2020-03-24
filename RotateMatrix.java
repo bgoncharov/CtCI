9550d0f1561192d945f397b96b87b1aa492f68a0
Boris Goncharov <bora4bora@gmail.com>
2020-03-23 18:23:56 -0700

    RotateMatrix

---
+++ RotateMatrix.java
@@ -0,0 +1,47 @@
+public class RotateMatrix {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < 10 && matrix[i][j] > -10) {
+                    System.out.print(" ");
+                }
+                if (matrix[i][j] < 100 && matrix[i][j] > -100) {
+                    System.out.print(" ");
+                }
+                if (matrix[i][j] >= 0) {
+                    System.out.print(" ");
+                }
+                System.out.print(" " + matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean rotate(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length) return false;
+        int n = matrix.length;
+
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i];
+                matrix[first][i] = matrix[last - offset][first];
+                matrix[last - offset][first] = matrix[last][last - offset];
+                matrix[last][last - offset] = matrix[i][last];
+                matrix[i][last] = top;
+            }
+        }
+        return true;
+    }
+
+    public static void main (String[]args){
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        printMatrix(matrix);
+        rotate(matrix);
+        System.out.println();
+        printMatrix(matrix);
+    }
+}
