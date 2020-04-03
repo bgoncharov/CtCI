af9fda1b6950eadc73c0bfc3b4f44ab785adb0b2
Boris Goncharov <bora4bora@gmail.com>
2020-04-02 18:42:10 -0700

    ListsPermutation

---
+++ ListsPermutation.java
@@ -0,0 +1,54 @@
+import CtCILibrary.LinkedListNode;
+
+import java.util.Stack;
+
+public class ListsPermutation {
+
+    public static boolean isPalindrome(LinkedListNode head) {
+        LinkedListNode fast = head;
+        LinkedListNode slow = head;
+
+        Stack <Integer> stack = new Stack<Integer>();
+
+        while (fast != null && fast.next != null) {
+            stack.push(slow.data);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (fast != null) {
+            slow = slow.next;
+        }
+
+        while (slow != null) {
+            int top = stack.pop().intValue();
+
+            if (top != slow.data) {
+                return false;
+            }
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int length = 9;
+        LinkedListNode[] nodes = new LinkedListNode[length];
+
+        for (int i = 0; i < length; i++) {
+            nodes[i] = new LinkedListNode(i >= length / 2 ? length - i - 1 : i, null, null);
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (i < length - 1) {
+                nodes[i].setNext(nodes[i + 1]);
+            }
+            if (i > 0) {
+                nodes[i].setPrevious(nodes[i - 1]);
+            }
+        }
+        LinkedListNode head = nodes[0];
+        System.out.println(head.printForward());
+        System.out.println(isPalindrome(head));
+    }
+}
