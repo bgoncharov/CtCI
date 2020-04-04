1482d63a9cd5f8fd06a4d6451efe82475cfd0d5e
Boris Goncharov <bora4bora@gmail.com>
2020-04-03 19:59:32 -0700

    stack

--- ThreeInOne.java
+++ ThreeInOne.java
@@ -1,3 +1,5 @@
+import CtCILibrary.AssortedMethods;
+
 import java.util.EmptyStackException;
 
 public class ThreeInOne {
@@ -13,7 +15,10 @@ public class ThreeInOne {
         sizes = new int[numberOfStacks];
     }
 
-    public void push(int stackNum, int value) throws {
+    public void push(int stackNum, int value) throws FullStackException {
+        if (isFull(stackNum)) {
+            throw new FullStackException();
+        }
         sizes[stackNum]++;
         values[indexOfTop(stackNum)] = value;
     }
@@ -51,7 +56,53 @@ public class ThreeInOne {
         return offset + size - 1;
     }
 
-    public static void main(String[] args) {
+    public int[] getValues() {
+        return values;
+    }
 
+    public int[] getStackValues(int stackNum) {
+        int[] items = new int[sizes[stackNum]];
+        for (int i = 0; i < items.length; i++) {
+            items[i] = values[stackNum * stackCap + i];
+        }
+        return items;
+    }
+
+    public static void printStacks(ThreeInOne stacks) {
+        System.out.println(AssortedMethods.arrayToString(stacks.getValues()));
+    }
+
+    public static void main(String [] args) throws Exception  {
+        ThreeInOne stacks = new ThreeInOne(4);
+        printStacks(stacks);
+        stacks.push(0, 10);
+        printStacks(stacks);
+        stacks.push(1, 20);
+        printStacks(stacks);
+        stacks.push(2, 30);
+        printStacks(stacks);
+
+        stacks.push(1, 21);
+        printStacks(stacks);
+        stacks.push(0, 11);
+        printStacks(stacks);
+        stacks.push(0, 12);
+        printStacks(stacks);
+
+        stacks.pop(0);
+        printStacks(stacks);
+
+        stacks.push(2, 31);
+        printStacks(stacks);
+
+        stacks.push(0, 13);
+        printStacks(stacks);
+        stacks.push(1, 22);
+        printStacks(stacks);
+
+        stacks.push(2, 31);
+        printStacks(stacks);
+        stacks.push(2, 32);
+        printStacks(stacks);
     }
 }
