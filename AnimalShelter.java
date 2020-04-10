a0df2787541c0e37b721513785d0d25d00ae5b34
Boris Goncharov <bora4bora@gmail.com>
2020-04-10 12:48:50 -0700

    AnimalShelter

---
+++ AnimalShelter.java
@@ -0,0 +1,22 @@
+public class AnimalShelter {
+
+    public void enqueue() {
+
+    }
+
+    public void dequeueAny() {
+
+    }
+
+    public void dequeueCat() {
+
+    }
+
+    public void dequeueDog() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
